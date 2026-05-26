25b3ceb28bdc974614b6b6ff991fe56c25ca1f7f
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-05-26 15:01:26 -0300

    feat(measures): added activity_level field to measure model

--- measures/models.py
+++ measures/models.py
@@ -14,6 +14,7 @@ class Measures(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Usuário')
     height = models.DecimalField(max_digits=5, decimal_places=2, verbose_name= 'Altura')
     weight = models.DecimalField(max_digits=5, decimal_places=2, verbose_name= 'Peso')
+    activity_level = models.IntegerField(default=1, verbose_name='Nível de atividade')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')
 
