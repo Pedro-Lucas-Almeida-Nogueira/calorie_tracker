4e0a83404943809a54e3747d0c197c11b1a60085
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-06-01 14:30:49 -0300

    feat(view): added get_queryset functions

--- measures/views.py
+++ measures/views.py
@@ -7,7 +7,13 @@ class MeasuresViewSet(viewsets.ModelViewSet):
     queryset = Measures.objects.all()
     serializer_class = MeasuresSerializer
 
+    def get_queryset(self):
+        return Measures.objects.filter(user=self.request.user)
+
 
 class UserAttributesListCreateView(generics.ListCreateAPIView):
     queryset = UserAttributes.objects.all()
     serializer_class = UserAttributesSerializer
+
+    def get_queryset(self):
+        return UserAttributes.objects.filter(user=self.request.user)
