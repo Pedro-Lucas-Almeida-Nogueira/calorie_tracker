cb501b434bb8750894683f57ab7f67544d58088a
Pedro Lucas <pedrolucasalmeidanogueira7@gmail.com>
2026-06-02 15:28:53 -0300

    feat(athentication): created urls

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('measures.urls'))
+    path('api/v1/', include('measures.urls')),
+    path('api/v1/auth/', include('authentication.urls'))
 ]
--- measures/views.py
+++ measures/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Measures, UserAttributes
 from .serializers import MeasuresSerializer, UserAttributesSerializer
 
 class MeasuresViewSet(viewsets.ModelViewSet):
     queryset = Measures.objects.all()
     serializer_class = MeasuresSerializer
+
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return Measures.objects.filter(user=self.request.user)
@@ -14,6 +17,8 @@ class MeasuresViewSet(viewsets.ModelViewSet):
 class UserAttributesListCreateView(generics.ListCreateAPIView):
     queryset = UserAttributes.objects.all()
     serializer_class = UserAttributesSerializer
+
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return UserAttributes.objects.filter(user=self.request.user)
